78873d937d09714f6b366a6e47a28eb7d6ee9fad
Paweł Rubach <pawel.rubach@gmail.com>
2021-11-08 10:15:40 +0100

    Added cmd test

--- test_runner.py
+++ test_runner.py
@@ -1,11 +1,8 @@
-import os.path
-
 from kafka_slurm_agent.command import Command
 
-#@pytest.mark.skip
-#def test_runner():
-#    cmd = Command('kafka-slurm create .')
-#    cmd.run(10)
-#    res = cmd.getOut()
-#    assert res is None
-#    assert os.path.isfile('kafkaslurm_cfg.py')
+def test_cmd():
+    #cmd = Command('kafka-slurm create .')
+    cmd = Command('uname')
+    cmd.run(10)
+    res = cmd.getOut()
+    assert res == 'Linux'
